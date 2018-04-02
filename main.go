18729a6398950e22cafe6ba2ad07f0f69f98c9d7
g4stly <g4stly@airmail.cc>
2018-04-02 14:59:48 -0400

    clean it up a little bit

--- main.go
+++ main.go
@@ -7,18 +7,20 @@ import (
 )
 
 func main() {
+
 	if len(os.Args) < 2 {
 		error_message := []byte(fmt.Sprintf("USAGE: %v <name>\n", os.Args[0]))
 		os.Stderr.Write(error_message)
 		os.Exit(-1)
 	}
 
-	pkmn := pokepull.Pull(os.Args[1])
-	json, err := pkmn.ToJson()
+	json, err := pokepull.Pull(os.Args[1]).ToJson()
+
 	if err != nil {
 		os.Stderr.Write([]byte(err.Error()))
-		return
+		os.Exit(-1)
 	}
+
 	os.Stdout.Write(json)
 	os.Exit(0)
 }
