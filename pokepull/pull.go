839de1bbf54b379200392f68545078f1d9e8c796
g4stly <g4stly@airmail.cc>
2018-04-03 21:23:01 -0400

    MAJOR refactor

--- pokepull/pull.go
+++ pokepull/pull.go
@@ -4,14 +4,22 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"errors"
 	"encoding/json"
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
-	"bytes"
 	"github.com/andelf/go-curl"
 )
 
 //==============================================================
+// Pokemon type and co.
+// innerStat, stat, and criticalStats are all types that
+// are used to translate strings stored as json to
+// primitives. this is why Pokemon type does not contain a
+// slice of stat types in favor of ints, against what you might
+// expect.
+//==============================================================
+
 type innerStat struct {
 	Name	string
 }
@@ -21,12 +29,12 @@ type stat struct {
 	Inner	innerStat	`json:"stat"`
 }
 
-type Pokemon struct {
+type criticalStats struct {
 	Name	string
 	Stats	[]stat		`json:"stats"`
 }
 
-type FmtPokemon struct {
+type Pokemon struct {
 	Name		string	`json:"name"`
 	Hp		int	`json:"hp"`
 	Attack		int	`json:"attack"`
@@ -37,65 +45,73 @@ type FmtPokemon struct {
 	stats		map[string]int
 }
 
-func (pkmn *Pokemon) ToFmtPokemon() *FmtPokemon {
-	new_pkmn := FmtPokemon{
-		Name: pkmn.Name,
+func (self *criticalStats) ToPokemon() *Pokemon {
+	pkmn := Pokemon {
+		Name: self.Name,
 		stats: make(map[string]int)}
-	for i := 0; i < len(pkmn.Stats); i++ {
-		target := pkmn.Stats[i].Inner.Name
-		switch(target) {
+
+	// loop over stats and copy as we go
+	for i:= 0; i < len(self.Stats); i++ {
+		stat_name := self.Stats[i].Inner.Name
+		switch(stat_name) {
 		case "attack":
-			new_pkmn.Attack = pkmn.Stats[i].Base
+			pkmn.Attack		= self.Stats[i].Base
 			break;
 		case "defense":
-			new_pkmn.Defense	= pkmn.Stats[i].Base
+			pkmn.Defense		= self.Stats[i].Base
 			break;
 		case "special-attack":
-			new_pkmn.Spdefense	= pkmn.Stats[i].Base
-			target = "spattack"
+			pkmn.Spdefense		= self.Stats[i].Base
+			stat_name = "spattack"
 			break;
 		case "special-defense":
-			new_pkmn.Spattack	= pkmn.Stats[i].Base
-			target = "spdefense"
+			pkmn.Spattack		= self.Stats[i].Base
+			stat_name = "spdefense"
 			break;
 		case "speed":
-			new_pkmn.Speed		= pkmn.Stats[i].Base
+			pkmn.Speed		= self.Stats[i].Base
 			break;
 		case "hp":
-			new_pkmn.Hp		= pkmn.Stats[i].Base
+			pkmn.Hp			= self.Stats[i].Base
 			break;
 		}
-		new_pkmn.stats[target] = pkmn.Stats[i].Base
+		pkmn.stats[stat_name] = self.Stats[i].Base
 	}
-	return &new_pkmn
+
+	return &pkmn
 }
 
-func (pkmn *FmtPokemon) Print() {
+func (pkmn *Pokemon) Print() {
 	fmt.Printf("format name: %v\n", pkmn.Name)
 	for key, value := range pkmn.stats {
 		fmt.Printf("%v: %v\n", key, value)
 	}
 }
 
-func (pkmn *FmtPokemon) ToJson() ([]byte, error) {
+func (pkmn *Pokemon) ToJson() ([]byte, error) {
 	output, err := json.Marshal(pkmn)
 	if err != nil {
 		return []byte(""), err
 	}
 	return output, nil
 }
+
+//==============================================================
+// PokemonJSON
+// this type is the representation of raw json data
 //==============================================================
 type PokemonJSON struct {
 	url	string
-	json	[]byte
+	rawJson	[]byte
 }
 
 func (self *PokemonJSON) Process(buffer []byte, userdata interface{}) bool {
-	self.json = append(self.json, buffer...)
+	self.rawJson = append(self.rawJson, buffer...)
 	return true
 }
 
-func (self *PokemonJSON) Fetch() error {
+func (self *PokemonJSON) Fetch(name string) *PokemonJSON {
+	log.Printf("now fetching pokemon: %v\n", name)
 	easy := curl.EasyInit()
 	defer easy.Cleanup()
 
@@ -103,61 +119,89 @@ func (self *PokemonJSON) Fetch() error {
 	easy.Setopt(curl.OPT_SSL_VERIFYPEER, false)
 	easy.Setopt(curl.OPT_WRITEFUNCTION, self.Process)
 
-	return easy.Perform()
+	err := easy.Perform()
+	if err != nil {
+		log.Fatalf("Fetch(): %v\n", err)
+	}
+
+	// after we've fetched the json,
+	// let's store it in our database
+	err = self.Store(name)
+	if err != nil {
+		log.Fatalf("Fetch(): %v\n", err)
+	}
+
+	// method chaining is sexy
+	return self
 }
 
-func (self *PokemonJSON) Parse() *Pokemon {
-	var pkmn Pokemon
-	log.Printf("parse(): %v", string(self.json))
-	json.Unmarshal(self.json, &pkmn)
-	return &pkmn
-}
-
-func (self *PokemonJSON) FmtParse() *FmtPokemon {
-	var pkmn FmtPokemon
-	json.Unmarshal(self.json, &pkmn)
-	return &pkmn
-}
-//==============================================================
-var baseURL = "https://pokeapi.co/api/v2/pokemon/"
-func Pull(name string) *FmtPokemon {
-	var json PokemonJSON
-	// do we have it in our database?
-	// first, open database
+func (self *PokemonJSON) Store(name string) error {
+	// open db connection
 	db, err := sql.Open("sqlite3", fmt.Sprintf("/home/%v/.pokepull/database.db", os.Getenv("USER")))
 	if err != nil { log.Fatal(err) }
 	defer db.Close();
-	// next, query for json
-	stmt, err := db.Prepare("SELECT json FROM pokemon WHERE name=?")
+
+	// sql statement 
+	stmt, err := db.Prepare("INSERT INTO pokemon (name, json) VALUES (?, ?);")
+	if err != nil { log.Fatalf("Store(): %v\n", err) }
+	defer stmt.Close()
+
+	// push the json inside the database 
+	res, err := stmt.Exec(name, self.rawJson)
+	if err != nil { return err }
+	numAffected, err := res.RowsAffected()
+	if err != nil { return err }
+	if numAffected != 1 { return errors.New("RowsAffected() did not return 1") }
+
+	return nil
+}
+func (self *PokemonJSON) Parse() *Pokemon {
+	// read into critical stats object
+	statsObj := criticalStats{}
+	err := json.Unmarshal(self.rawJson, &statsObj)
+	if err != nil { log.Fatal(err) }
+
+	return statsObj.ToPokemon()
+}
+
+//==============================================================
+// Main package functions
+//==============================================================
+func pullFromDB(name string) (*Pokemon, bool) {
+	var json PokemonJSON
+
+	// open db connection
+	db, err := sql.Open("sqlite3", fmt.Sprintf("/home/%v/.pokepull/database.db", os.Getenv("USER")))
+	if err != nil { log.Fatal(err) }
+	defer db.Close();
+
+	// sql statement 
+	stmt, err := db.Prepare("SELECT json FROM pokemon WHERE name=?;")
 	if err != nil { log.Fatal(err) }
 	defer stmt.Close()
-	err = stmt.QueryRow(name).Scan(&json.json)
-	if err != nil && err != sql.ErrNoRows { // if we error'd out
+
+	// pull the json out of the db
+	err = stmt.QueryRow(name).Scan(&json.rawJson)
+	if err != nil && err != sql.ErrNoRows {
+		// if we error'd out
 		log.Fatal(err)
-	} else if err == nil {			// if we did not error out
-		return json.FmtParse()
-	}					// if we error'd out cuz there was no entry
-
-	// set up url
-	var url bytes.Buffer
-	for i := 0; i < len(baseURL); i++ { url.WriteByte(baseURL[i]) }
-	url.WriteString(fmt.Sprintf("%v/", name))
-
-
-	json.url = url.String()
-	json.Fetch()
-	pkmn := json.Parse()
-	fmt_pkmn := pkmn.ToFmtPokemon()
-
-	// insert new formatted pokemon into database
-	insertStmt, err := db.Prepare("INSERT INTO pokemon (name, json) VALUES (?, ?);")
-	if err != nil { log.Fatal(err) }
-	defer insertStmt.Close()
-	literalJson, err := fmt_pkmn.ToJson()
-	if err != nil { log.Fatal(err) }
-	_ , err = insertStmt.Exec(fmt_pkmn.Name, literalJson)
-	if err != nil {
-		log.Fatal(err)
+	} else if err == nil {
+		// if we did not error out
+		return json.Parse(), true
 	}
-	return fmt_pkmn
+	// if we error'd out cuz there was no entry
+	return nil, false
+}
+
+var baseURL = "https://pokeapi.co/api/v2/pokemon/"
+
+func Pull(name string) *Pokemon{
+	// attempt to pull stats from our local database
+	pkmn, ok := pullFromDB(name)
+	if ok { return pkmn }
+
+	// things are !ok, pull from pokeapi.co
+	var json PokemonJSON
+	json.url = fmt.Sprintf("%v%v/", baseURL, name)
+	return json.Fetch(name).Parse()
 }
